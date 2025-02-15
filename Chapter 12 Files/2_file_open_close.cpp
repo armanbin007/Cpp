ec375c544d4f7e06446a43b2de0a7e8a7e0e7ab2
Arman Bin Alauddin <160709760+armanbin007@users.noreply.github.com>
2025-02-16 01:48:50 +0600

    Add files via upload

--- Chapter 12 Files/2_file_open_close.cpp
+++ Chapter 12 Files/2_file_open_close.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
     // connecting our file with hout stream
-    ofstream hout("sample1.txt"); // Openning file using constructor
+    ofstream hout("sample3.txt"); // Openning file using constructor
     // creating a name string variable and filling it with string entered by the user
     string name;
     cout << "Enter name: ";
@@ -14,7 +14,7 @@ int main(){
     hout << "Name: " + name;
     hout.close();
 
-    ifstream hin("sample1.txt");
+    ifstream hin("sample4.txt");
     string content;
     // hin >> content; // Works like cin
     getline(hin, content); // Gets the whole string
